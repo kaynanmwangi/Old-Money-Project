0a2649a91ee52a4e4c76e451c692ae6b4d1c63a0
kaynanmwangi <kaynan.mwangi@student.moringaschool.com>
2025-05-01 13:34:59 +0300

    save changes

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
-    // Basic JavaScript for potential interactivity (e.g., button click alerts for now)
-    document.querySelectorAll('.cta').forEach(button => {
-        button.addEventListener('click', (e) => {
-          e.preventDefault();
-          alert(`Navigating to ${e.target.textContent}`);
-        });
-      });
+    // // Basic JavaScript for potential interactivity (e.g., button click alerts for now)
+    // document.querySelectorAll('.cta').forEach(button => {
+    //     button.addEventListener('click', (e) => {
+    //       e.preventDefault();
+    //       alert(`Navigating to ${e.target.textContent}`);
+    //     });
+    //   });
 
 
     
